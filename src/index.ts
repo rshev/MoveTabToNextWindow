676f6e513f44700b8e1e1a6044fe8b2d79bf083b
Roman Shevtsov <roman.shevtsov@gmail.com>
2020-06-04 19:40:23 +0100

    Adds support for moving multiple highlighted tabs.

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,29 @@ class Extension {
     try {
       browser.contextMenus.create({
         contexts: ["tab"],
-        onclick: (_, tab) => this.moveTab(tab),
+        onclick: (_, tab) => this.moveTabOrHighlightedTabs(tab),
         title: "Move to the next window",
       });
     } catch {
       console.log("oops, Chrome doesn't support extensions in tab menus (yet)");
     }
-    browser.browserAction.onClicked.addListener((tab) => this.moveTab(tab));
+    browser.browserAction.onClicked.addListener((tab) =>
+      this.moveTabOrHighlightedTabs(tab)
+    );
+  }
+
+  private async moveTabOrHighlightedTabs(tab: Tabs.Tab) {
+    const highlightedTabs = await browser.tabs.query({
+      highlighted: true,
+      currentWindow: true,
+    });
+    if (highlightedTabs.length > 1) {
+      for (const tab of highlightedTabs) {
+        await this.moveTab(tab);
+      }
+    } else {
+      this.moveTab(tab);
+    }
   }
 
   private async moveTab(tab: Tabs.Tab) {
